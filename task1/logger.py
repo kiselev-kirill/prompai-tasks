6072c510a3eca0a23b9faf8f6ae5ddb0a2458f43
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2025-10-20 17:52:41 +0300

    add logger for task 1

--- task1/logger.py
+++ task1/logger.py
@@ -6,27 +6,30 @@ class DictToPgLogger:
     Класс для логирования.
     Поддерживает уровни INFO и ERROR, кастомное форматирование.
     """
-    def __init__(self, name: str | None = None, level: int = logging.INFO):
+
+    def __init__(self, name: str | None = None):
         """
         Инициализация логгера.
 
         :param name: имя логгера
-        :param level: уровень логирования (по умолчанию INFO)
         """
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
 
-        if not self.logger.handlers:
-            handler = logging.StreamHandler()
-            handler.setLevel(level)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(name)s] %(levelname)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-            # Форматтер наших логов
-            formatter = logging.Formatter(
-                "[%(asctime)s] [%(name)s] %(levelname)s: %(message)s",
-                datefmt="%Y-%m-%d %H:%M:%S",
-            )
-            handler.setFormatter(formatter)
-            self.logger.addHandler(handler)
+    def info(self, msg: str, *args, **kwargs):
+        """Логировать информационное сообщение"""
+        self.logger.info(msg, *args, **kwargs)
+
+    def error(self, msg: str, *args, **kwargs):
+        """Логировать сообщение об ошибке"""
+        self.logger.error(msg, *args, **kwargs)
 
     @property
     def get_logger(self) -> logging.Logger:
@@ -34,5 +37,5 @@ class DictToPgLogger:
         return self.logger
 
 
-# глобальный логгер
-logger = DictToPgLogger("dict_to_pg").get_logger
+# глобальный логгер для задания 1
+logger = DictToPgLogger("dict_to_pg")
